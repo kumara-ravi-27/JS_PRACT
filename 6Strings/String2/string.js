1d53e3aadedcc8ded6c6b7d306826a306591e9c0
kumara_ravi_27 <145559569+kumara-ravi-27@users.noreply.github.com>
2023-10-13 21:16:38 +0530

    Create string.js

---
+++ 6Strings/String2/string.js
@@ -0,0 +1,64 @@
+function myFun3(){
+
+  let t21=
+  'helo ravi';
+
+  let t22='helo    ravi';
+
+  let t23='helo \
+  ravi';
+
+  let t24='helo'+ 'ravi';
+
+  let t25= 'helo ravi';
+
+  let t26= new String('kumar');
+
+  let t27= typeof t21 + typeof t26;
+
+  document.getElementById('d21').innerHTML=t21;
+  document.getElementById('d22').innerHTML=t22;
+  document.getElementById('d23').innerHTML=t23;
+  document.getElementById('d24').innerHTML=t24;
+  document.getElementById('d25').innerHTML=t25;
+  document.getElementById('d26').innerHTML=t26;
+  document.getElementById('d27').innerHTML=t27;
+  
+}
+
+/*
+helo ravi
+helo ravi
+helo ravi
+heloravi
+helo ravi
+ravi
+kumar
+stringobject
+*/
+
+
+/*
+string methods
+String length
+String slice()
+String substring()
+String substr()
+String replace()
+String replaceAll()
+String toUpperCase()
+String toLowerCase()
+String concat()
+String trim()
+String trimStart()
+String trimEnd()
+String padStart()
+String padEnd()
+String charAt()
+String charCodeAt()
+String split()
+
+*/
+
+
+
